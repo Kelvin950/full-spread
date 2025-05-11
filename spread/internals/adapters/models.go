914a5fde89d77962414c77af890be19eee8a8ec6
kelvin <denlinato@gmail.com>
2025-05-11 13:47:53 +0000

    ficed

--- spread/internals/adapters/models.go
+++ spread/internals/adapters/models.go
@@ -11,3 +11,18 @@ type UplaodMultiPartReq struct {
 	Key        string `json:"key" binding:"required"`
 	PartNumber []int  `json:"part_number" binding:"required"`
 }
+
+
+type CompleteMultiPartReq struct {
+	Bucket  string `json:"bucket"`
+	UploadId string `json:"upload_id"`
+	Key      string `json:"key"`
+   Parts []Parts `json:"parts"`
+}
+
+
+
+type Parts struct {
+	Etag       *string `json:"etag"`
+	PartNumber *int32  `json:"part_number"`
+}
